a1b3179fc2c666a1f8caf45f107388acd1cfa49d
Are Hansen <info@bifrozt.com>
2016-02-29 15:03:27 +0100

    bifrozt_setup.sh: 0.0.3-DEV

--- bifrozt_setup.sh
+++ bifrozt_setup.sh
@@ -25,8 +25,35 @@
 #   WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 #
+#   --------------------------------------------------------------
+#
+#   - 0.0.3-DEV
+#     * Added change log data
+#     * Removing the neccesity of using 'sudo su -' by generating the Ansible SSH key in a
+#       custom location and setting the 'private_key_file' in ansible.cfg.
+#     * Added 'info' argument.
+#     * Supressed output from various commands.
+#
+#   --------------------------------------------------------------
+#
 declare -rx Script="${0##*/}"
+declare created="2016, Feb 24"
+declare author="Are Hansen"
+declare version="0.0.3-DEV"
 
+
+# Outputs information about this script and exits.
+function script_info()
+{
+echo "
+    ========= $Script - $version - $created - $author =========
+
+    This script a part of the Bifrozt honeypot router project. It's been 
+    developed to assist end users to install the required software and
+    configuration files in a quick and easy manner.
+
+"
+}
 
 # Verify that the distro we are using is Ubuntu
 function check_distro()
@@ -57,31 +84,43 @@ function apt_get_things()
 # Install Ansible dependencies.
 function install_ansible()
 {
-    apt-get install software-properties-common -y
-    apt-add-repository ppa:ansible/ansible -y
+    apt-get install software-properties-common -y &>/dev/null
+    apt-add-repository ppa:ansible/ansible -y &>/dev/null
     apt-get update &>/dev/null
     apt-get install ansible git openssh-server -y &>/dev/null
     sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
 }
 
 
-# Cloning bifrozt-ansible into /root/bifrozt-ansible
+# Cloning bifrozt-ansible into /tmp/bifrozt-ansible
 function git_clone()
 {
-    git clone https://github.com/Bifrozt/bifrozt-ansible.git /root/bifrozt-ansible
+    git clone https://github.com/Bifrozt/bifrozt-ansible.git /tmp/bifrozt-ansible &>/dev/null
 }
 
 
 # Generate SSH keys for root
 function gen_ssh_keys()
 {
-    if [ ! -d "/root/.ssh" ]
+    if [ ! -d "/etc/ansible/BZKEY" ]
     then
-        mkdir "/root/.ssh"
-        chmod 0700 "/root/.ssh"
-        chown root:root "/root/.ssh"
+        mkdir "/etc/ansible/BZKEY"
+        chmod 0700 "/etc/ansible/BZKEY"
+        chown root:root "/etc/ansible/BZKEY"
     fi
-    ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
+
+    ssh-keygen -f /etc/ansible/BZKEY/id_rsa -t rsa -N '' &>/dev/null
+
+    if [ ! -e "/etc/ansible/BZKEY/id_rsa.pub" ]
+    then
+        echo "$(date +"%Y-%m-%d %T") - FAIL: id_rsa.pub not found in expected location."
+    	exit 1
+    else
+        echo "$(date +"%Y-%m-%d %T") - INFO: Generating /root/.ssh/authorized_keys..."
+        cat "/etc/ansible/BZKEY/id_rsa.pub" > "/root/.ssh/authorized_keys"
+    	chmod 0600 "/etc/ansible/BZKEY/id_rsa.pub"
+        sed -i 's/#private_key_file = \/path\/to\/file/private_key_file = \/etc\/ansible\/BZKEY\/id_rsa/g' /etc/ansible/ansible.cfg 
+    fi
 }
 
 
@@ -149,21 +188,24 @@ function main()
     echo "$(date +"%Y-%m-%d %T") - INFO: Generating SSH keys..."
     gen_ssh_keys
 
-    echo "$(date +"%Y-%m-%d %T") - INFO: Generating /root/.ssh/authorized_keys..."
-    cat "/root/.ssh/id_rsa.pub" > ".ssh/authorized_keys"
-
     echo "$(date +"%Y-%m-%d %T") - INFO: Executing the playbook now..."
     IPV4="$(find_host_ipv4)"
-    sed -i "s/IPv4_OR_FQDN/$IPV4/g" /root/bifrozt-ansible/hosts
+    sed -i "s/IPv4_OR_FQDN/$IPV4/g" /tmp/bifrozt-ansible/hosts
     # If the playbook returns a non-zero exit code, exit 1, if returning zero, sleep 10 seconds and rebboot
-    ansible-playbook /root/bifrozt-ansible/playbook.yml -i /root/bifrozt-ansible/hosts \
+    ansible-playbook /tmp/bifrozt-ansible/playbook.yml -i /tmp/bifrozt-ansible/hosts \
     && reboot_system \
     || exit 1
 }
 
 
-# Starts execution of script
-main
+case "$1" in
+    info)
+        script_info
+        ;;
+    *)
+        main
+        ;;
+esac
 
 
 exit 0
